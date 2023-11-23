251b3c4445b3549556b40c9db42784a1f817df29
Julia Stella <julia.paula@liferay.com>
2023-11-22 22:36:34 -0300

    Quebrando a banca atual

--- Execicios/quebrando_a_Banca.py
+++ Execicios/quebrando_a_Banca.py
@@ -1,21 +1,22 @@
 # Função para encontrar o maior saldo possível após remover dígitos
 def encontrar_maior_saldo(A, B, saldo):
-    saldo_lista = list(map(int, saldo))
-    saldo_ordenado = sorted(saldo_lista, reverse=True)
-    saldo_final = saldo_ordenado[B:]
-    return ''.join(map(str, saldo_final))
-
+   saldo_lista = list(map(int, saldo))
+   saldo_ordenado = sorted(saldo_lista, reverse=True)
+   if B > len(saldo_ordenado):
+       B = len(saldo_ordenado)
+   saldo_final = saldo_ordenado[:-B]
+   return ''.join(map(str, saldo_final))
 
 # Processar cada caso de teste
 while True:
-    entrada = input().split()
-    if len(entrada) == 0:
-        break
-    A, B = map(int, entrada)
-    saldo = input()
-    
-    # Chamar a função para encontrar o maior saldo possível
-    resultado = encontrar_maior_saldo(A, B, saldo)
+   entrada = input().split()
+   if len(entrada) == 0:
+       break
+   A, B = map(int, entrada)
+   saldo = input()
+   
+   # Chamar a função para encontrar o maior saldo possível
+   resultado = encontrar_maior_saldo(A, B, saldo)
 
-    # Imprimir o resultado
-    print(resultado)
+   # Imprimir o resultado
+   print(resultado)
